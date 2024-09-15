bf8f7e1e4ff1eaa2a8d0f169c2f0f26b099e1234
ViolinistaEstudante <132099734+ViolinistaEstudante@users.noreply.github.com>
2024-09-15 12:08:54 -0300

    Site concluído!

---
+++ dados.js
@@ -0,0 +1,28 @@
+let dados = [
+    {
+      titulo: "O que é Setembro Amarelo?",
+      descricao: "Entenda o propósito e a importância da campanha Setembro Amarelo para a prevenção do suicídio e promoção da saúde mental.",
+      link: "https://www.septembroamarelo.com.br/"
+    },
+    {
+      titulo: "Como Identificar Sinais de Alerta",
+      descricao: "Saiba como reconhecer os sinais de alerta de alguém que pode estar passando por uma crise emocional e como agir para ajudar.",
+      link: "https://www.apm.org.br/o-que-diz-a-midia/setembro-amarelo-quais-sao-os-sinais-de-alerta-da-depressao/"
+    },
+    { 
+      titulo: "A Importância do Apoio Psicológico",
+      descricao: "Descubra por que buscar apoio psicológico é fundamental para a prevenção do suicídio e como encontrar ajuda profissional.",
+      link: "https://www.itausocial.org.br/noticias/apoio-psicologico-na-escola-torna-estudantes-mais-felizes-e-motivados-aponta-pesquisa/#:~:text=De%20acordo%20com%20especialistas%2C%20o,suporte%20ao%20desempenho%20no%20aprendizado."
+    },
+    {
+      titulo: "Estratégias de Prevenção",
+      descricao: "Entenda as estratégias eficazes para prevenir o suicídio e promover o bem-estar mental na sua comunidade.",
+      link: "https://www.gov.br/saude/pt-br/assuntos/saude-de-a-a-z/s/suicidio-prevencao"
+    },
+    {
+      titulo: "Mitos e Verdades sobre Suicídio",
+      descricao: "Desmistifique os conceitos errôneos sobre o suicídio e conheça as verdades que podem ajudar a combater o estigma.",
+      link: "https://gauchazh.clicrbs.com.br/saude/noticia/2022/05/mitos-e-verdades-sobre-o-suicidio-cl2s45qvp00b1019i45thi66f.html"
+    }
+  ]
+  
